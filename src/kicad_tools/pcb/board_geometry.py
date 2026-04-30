b425abc9b8f6c548f417f291985c525d5990aabd
Robb Walters <robb@2amlogic.com>
2026-04-30 17:50:28 +0200

    Remove unused 'field' import (during evaluation)

--- src/kicad_tools/pcb/board_geometry.py
+++ src/kicad_tools/pcb/board_geometry.py
@@ -22,7 +22,7 @@ Usage::
 from __future__ import annotations
 
 import math
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
